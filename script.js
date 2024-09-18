a32ed691db99ef5bf4b9201edc08fb6cc92222aa
slayed-coder <101445508+slay3r2k22@users.noreply.github.com>
2024-09-18 19:25:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ window.addEventListener('scroll', function() {
     const scrolled = window.pageYOffset;
     document.body.style.backgroundPositionY = `${scrolled * 0.5}px`;
 });
+
+function toggleNav() {
+    const navbar = document.getElementById("navbar");
+    if (navbar.style.display === "flex") {
+        navbar.style.display = "none";
+    } else {
+        navbar.style.display = "flex";
+    }
+}
